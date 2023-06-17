dd97f959a8a922d86fb37f75698e68620e4fa4bb
Joshimello <joshualeanjw@gmail.com>
2023-06-18 01:12:46 +0800

    cleanup

--- src/player/atsuko.cpp
+++ src/player/atsuko.cpp
@@ -63,28 +63,16 @@ void write_valid_spot(ofstream& fout) {
   int moveindex = 0;
   int bestmoveindex = 0;
 
-  cout << "Evals: ";
-
   for(auto& move : root->legal_actions){
     int eval = get_move_val(root->next_state(move), 6, -1e9, 1e9);
-    cout << eval << ' ';
-
     if(eval > besteval){
       besteval = eval;
       bestmoveindex = moveindex;
     }
-
     moveindex++;
   }
 
-  cout << endl;
-
   Move move = root->legal_actions[bestmoveindex];
-
-  cout << besteval << ' ' << bestmoveindex << endl;
-  cout << move.first.first << " " << move.first.second << " "\
-       << move.second.first << " " << move.second.second << endl;
-
   fout << move.first.first << " " << move.first.second << " "\
        << move.second.first << " " << move.second.second << endl;
   
@@ -95,12 +83,8 @@ int main(int, char** argv) {
   ifstream fin(argv[1]);
   ofstream fout(argv[2]);
 
-  cout << "_______________________" << endl;
-
   read_board(fin);
   write_valid_spot(fout);
-
-  cout << "_______________________" << endl;
 
   fin.close();
   fout.close();
